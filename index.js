3c412e8da8f6ebc4b841a46993341d83b47c9ad1
mahmudulkabir <mahmudulkabir990@gmail.com>
2022-05-10 10:20:11 +0600

    services/:id api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,12 +14,20 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('fruitsValley').collection('services');
+
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
 
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
         })
     }
     finally {

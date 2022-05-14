a819334bc56919978a52d67a651ea10ae5940c44
mahmudulkabir <mahmudulkabir990@gmail.com>
2022-05-14 12:52:27 +0600

    error solved

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
